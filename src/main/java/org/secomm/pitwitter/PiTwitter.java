da2a202fe55e51e850708692ebdd7d94fb12113c
Steve Brenneis <swbrenneis@gmail.com>
2021-07-15 20:49:19 -0400

    Handover commit.

--- src/main/java/org/secomm/pitwitter/PiTwitter.java
+++ src/main/java/org/secomm/pitwitter/PiTwitter.java
@@ -1,6 +1,32 @@
 package org.secomm.pitwitter;
 
-public class PiTwitter {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class PiTwitter implements CommandLineRunner {
 
+    private static final Logger log = LoggerFactory.getLogger(PiTwitter.class);
+
+    @Autowired
+    private TwitterHandler twitterHandler;
+
+    public static void main(String... args) {
+        SpringApplication.run(PiTwitter.class);
+    }
+
+    @Override
+    public void run(String... args) {
+
+        try {
+            twitterHandler.initialize();
+            twitterHandler.run();
+        } catch (Exception e) {
+            log.error("Exception caught in run method: {}", e.getLocalizedMessage());
+        }
+    }
 }
